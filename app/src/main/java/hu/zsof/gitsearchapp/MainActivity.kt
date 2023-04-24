5bcdd0a2f965dde6227a228a6a96796f39e9af14
zsof <zsofi.viki@gmail.com>
2023-04-24 15:20:27 +0200

    Initial commit

---
+++ app/src/main/java/hu/zsof/gitsearchapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package hu.zsof.gitsearchapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
