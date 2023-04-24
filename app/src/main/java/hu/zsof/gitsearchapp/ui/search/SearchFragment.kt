2dffa8b21be2b1b1e93c105cf12a03b09e5a633a
zsof <zsofi.viki@gmail.com>
2023-04-24 16:21:55 +0200

    Create search fragment layout and item layout

--- app/src/main/java/hu/zsof/gitsearchapp/ui/search/SearchFragment.kt
+++ app/src/main/java/hu/zsof/gitsearchapp/ui/search/SearchFragment.kt
@@ -1,7 +1,42 @@
 package hu.zsof.gitsearchapp.ui.search
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
+import hu.zsof.gitsearchapp.R
+import hu.zsof.gitsearchapp.adapter.SearchAdapter
+import hu.zsof.gitsearchapp.databinding.FragmentSearchBinding
 
 @AndroidEntryPoint
-class SearchFragment : Fragment()
+class SearchFragment : Fragment() {
+
+    private lateinit var binding: FragmentSearchBinding
+    private lateinit var adapter: SearchAdapter
+    private val viewModel: SearchViewModel by viewModels()
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_search, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupBindings()
+    }
+
+    private fun setupBindings() {
+        binding.apply {
+            searchButton.setOnClickListener {
+            }
+        }
+    }
+}
