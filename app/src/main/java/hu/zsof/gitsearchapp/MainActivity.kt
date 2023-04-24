818aebb159fc081952baeffb9c8e2d011ea7d44f
zsof <zsofi.viki@gmail.com>
2023-04-24 15:48:38 +0200

    Add navigation

--- app/src/main/java/hu/zsof/gitsearchapp/MainActivity.kt
+++ app/src/main/java/hu/zsof/gitsearchapp/MainActivity.kt
@@ -2,12 +2,29 @@ package hu.zsof.gitsearchapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import dagger.hilt.android.AndroidEntryPoint
+import hu.zsof.gitsearchapp.databinding.ActivityMainBinding
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var navHostFragment: NavHostFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupNavController()
+    }
+
+    private fun setupNavController() {
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+
+        navController = navHostFragment.navController
     }
 }
